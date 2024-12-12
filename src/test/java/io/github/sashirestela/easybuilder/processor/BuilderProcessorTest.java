445a8e6246b44f563e42759e3b8daf332be2fd1c
Sashir Estela <sashirestela@gmail.com>
2024-12-12 21:35:04 +0000

    Improve unit testing

--- src/test/java/io/github/sashirestela/easybuilder/processor/BuilderProcessorTest.java
+++ src/test/java/io/github/sashirestela/easybuilder/processor/BuilderProcessorTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class BuilderProcessorTest {
 
     private CuteApi.BlackBoxTestSourceFilesInterface compileTestBuilder;
@@ -20,27 +22,138 @@ class BuilderProcessorTest {
     }
 
     @Test
-    void test_valid_usage() {
+    void shouldCompileFromStandardRecordAndTestFreshRecordFromBuilder() {
+        String originalFile = "com.mycompany.demo.model.StandardRecord";
+        String generatedFile = "com.mycompany.demo.model.StandardRecordBuilder";
         compileTestBuilder
-                .andSourceFiles("testcases/Item.java.ct")
+                .andSourceFiles("testcases/StandardRecord.java.ct")
                 .whenCompiled()
                 .thenExpectThat()
                 .compilationSucceeds()
                 .andThat()
-                .compilerMessage()
-                .ofKindNote()
-                .contains("Generating from Class io.github.sashirestela.easybuilder.Item")
-                .andThat()
-                .compilerMessage()
-                .ofKindNote()
-                .contains("Generating from Class io.github.sashirestela.easybuilder.Item.SubItem")
-                .andThat()
-                .generatedSourceFile("io.github.sashirestela.easybuilder.ItemBuilder")
+                .generatedSourceFile(generatedFile)
                 .exists()
                 .andThat()
-                .generatedSourceFile("io.github.sashirestela.easybuilder.SubItemBuilder")
+                .generatedClassesTestedSuccessfullyBy(cuteClassLoader -> {
+                    String[] methods = { "id", "name", "amount" };
+                    Class<?>[] classes = { Integer.class, String.class, Double.class };
+                    Object[] values = { 101, "Sample", 17.65 };
+
+                    Class<?> recordClass = cuteClassLoader.getClass(originalFile);
+                    Object expectedRecord = newObjectFromClass(recordClass, classes, values);
+
+                    Class<?> builderClass = cuteClassLoader.getClass(generatedFile);
+                    Object actualRecord = newFreshObjectFromBuilder(builderClass, methods, classes, values);
+
+                    assertEquals(expectedRecord, actualRecord);
+                })
+                .executeTest();
+    }
+
+    @Test
+    void shouldCompileFromStandardRecordAndTestStaleRecordFromBuilder() {
+        String originalFile = "com.mycompany.demo.model.StandardRecord";
+        String generatedFile = "com.mycompany.demo.model.StandardRecordBuilder";
+        compileTestBuilder
+                .andSourceFiles("testcases/StandardRecord.java.ct")
+                .whenCompiled()
+                .thenExpectThat()
+                .compilationSucceeds()
+                .andThat()
+                .generatedSourceFile(generatedFile)
+                .exists()
+                .andThat()
+                .generatedClassesTestedSuccessfullyBy(cuteClassLoader -> {
+                    String[] methods = { "id", "name", "amount" };
+                    Class<?>[] classes = { Integer.class, String.class, Double.class };
+                    Object[] values1 = { 101, "Expected", 17.65 };
+                    Object[] values2 = { 101, "Temporal", 17.65 };
+
+                    Class<?> recordClass = cuteClassLoader.getClass(originalFile);
+                    Object expectedRecord = newObjectFromClass(recordClass, classes, values1);
+                    Object temporalRecord = newObjectFromClass(recordClass, classes, values2);
+
+                    Class<?> builderClass = cuteClassLoader.getClass(generatedFile);
+                    Object actualRecord = newStaleObjectFromBuilder(builderClass, methods[1], classes[1], values1[1],
+                            recordClass, temporalRecord);
+
+                    assertEquals(expectedRecord, actualRecord);
+                })
+                .executeTest();
+    }
+
+    @Test
+    void shouldCompileFromCompositeRecordAndTestNestedRecord() {
+        String originalFile = "com.mycompany.demo.model.CompositeRecord$IntermediateRecord$NestedRecord";
+        String generatedFile = "com.mycompany.demo.model.NestedRecordBuilder";
+        compileTestBuilder
+                .andSourceFiles("testcases/CompositeRecord.java.ct")
+                .whenCompiled()
+                .thenExpectThat()
+                .compilationSucceeds()
+                .andThat()
+                .generatedSourceFile(generatedFile)
+                .exists()
+                .andThat()
+                .generatedClassesTestedSuccessfullyBy(cuteClassLoader -> {
+                    String[] methods = { "id", "detail" };
+                    Class<?>[] classes = { Integer.class, String.class };
+                    Object[] values = { 101, "Sample" };
+
+                    Class<?> recordClass = cuteClassLoader.getClass(originalFile);
+                    Object expectedRecord = newObjectFromClass(recordClass, classes, values);
+
+                    Class<?> builderClass = cuteClassLoader.getClass(generatedFile);
+                    Object actualRecord = newFreshObjectFromBuilder(builderClass, methods, classes, values);
+
+                    assertEquals(expectedRecord, actualRecord);
+                })
+                .executeTest();
+    }
+
+    @Test
+    void shouldCompileFromEmptyRecord() {
+        String generatedFile = "com.mycompany.demo.model.EmptyRecordBuilder";
+        compileTestBuilder
+                .andSourceFiles("testcases/EmptyRecord.java.ct")
+                .whenCompiled()
+                .thenExpectThat()
+                .compilationSucceeds()
+                .andThat()
+                .generatedSourceFile(generatedFile)
                 .exists()
                 .executeTest();
+    }
+
+    private Object newObjectFromClass(Class<?> clazz, Class<?>[] classes, Object[] values)
+            throws Exception {
+        return Util.instance(Util.constructor(clazz, classes), values);
+    }
+
+    private Object newFreshObjectFromBuilder(Class<?> builderClass, String[] methods, Class<?>[] classes,
+            Object[] values) throws Exception {
+        return newObjectFromBuilder(builderClass, methods, classes, values, null, null);
+    }
+
+    private Object newStaleObjectFromBuilder(Class<?> builderClass, String method, Class<?> clazz, Object value,
+            Class<?> baseClass, Object baseObject) throws Exception {
+        return newObjectFromBuilder(builderClass, new String[] { method }, new Class[] { clazz },
+                new Object[] { value }, baseClass, baseObject);
+    }
+
+    private Object newObjectFromBuilder(Class<?> builderClass, String[] methods, Class<?>[] classes, Object[] values,
+            Class<?> baseClass, Object baseObject) throws Exception {
+        Object[] EMPTY = new Object[0];
+        Object builderObject = null;
+        if (baseClass == null) {
+            builderObject = Util.invoke(Util.method(builderClass, "builder"), null, EMPTY);
+        } else {
+            builderObject = Util.invoke(Util.method(builderClass, "builder", baseClass), null, baseObject);
+        }
+        for (int i = 0; i < methods.length; i++) {
+            builderObject = Util.invoke(Util.method(builderClass, methods[i], classes[i]), builderObject, values[i]);
+        }
+        return Util.invoke(Util.method(builderClass, "build"), builderObject, EMPTY);
     }
 
 }
